ea843a6fb19cc3072985970cfc31bb1bee135085
Naz Khan <nazmuskhan@yahoo.com>
2017-07-29 18:41:53 -0400

    complete

--- hashTable.js
+++ hashTable.js
@@ -47,6 +47,32 @@ HashTable.prototype.insert = function(key, value) {
   }
 }
 
+// extract particular node by key name
+HashTable.prototype.get = function(key) {
+  let index = this.hash(key)
+  if (!this.buckets[index]) return null
+  let currentNode = this.buckets[index]
+  while (currentNode) {
+    if (currentNode.key === key) return currentNode.value
+    currentNode = currentNode.next
+  }
+  return null
+}
+
+// iterates through all buckets, and checks for chains in each bucket - returns array of all hash nodes
+HashTable.prototype.retrieveAll = function() {
+  let allNodes = []
+
+  for (let i = 0; i < this.numBuckets; i++) {
+    let currentNode = this.buckets[i]
+    while (currentNode) {
+      allNodes.push(currentNode)
+      currentNode = currentNode.next
+    }
+  }
+  return allNodes
+}
+
 // test nodes
 myHT.insert("Nael", 'chim4ira312@gmail.com')
 myHT.insert("Melissa", 'cantoralm@ymail.com')
