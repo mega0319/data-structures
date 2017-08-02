6cb7dfe5e9dffcc5d238ef3a19e6e1f0d31d42c1
Naz Khan <nazmuskhan@yahoo.com>
2017-08-02 15:03:39 -0400

    complete

--- stack.js
+++ stack.js
@@ -1,14 +1,20 @@
 // Stacks
 
+// O(1) or constant time to add item to a stack
+// O(n) to search for item through stack
+
+// Stack constructor
 function Stack() {
   this.top = null
 }
 
+// Stack node constructor
 function StackNode(data) {
   this.data = data
   this.next = null
 }
 
+// test subjects
 let newStack = new Stack()
 let newNode = newStackNode(10)
 let anotherNode = newStackNode(6)
@@ -16,6 +22,7 @@ let anotherNode = newStackNode(6)
 newStack.top = newNode
 newNode.next = anotherNode
 
+// Pop function removes the head node and sets a new head for the stack in constant time
 Stack.prototype.pop = function() {
   if (this.top === null) {
     return -1
@@ -27,6 +34,7 @@ Stack.prototype.pop = function() {
   return item
 }
 
+// Pushing node onto the top of the stack - constant time
 Stack.prototype.push = function(data) {
   if (this.top === null) {
     this.top = node
@@ -35,4 +43,28 @@ Stack.prototype.push = function(data) {
 
   newNode.next = this.top
   this.top = newNode
+}
+
+// Returns the top node in stack
+Stack.prototype.peek = function() {
+  if (this.top === null) {
+    return -1
+  }
+  return this.top.data
+}
+
+// Returns boolean by checking to see if stack is empty
+Stack.prototype.isEmpty = function() {
+  return this.top === null
+}
+
+Stack.prototype.search = function(searchValue) {
+  let currentNode = this.top
+
+  while (currentNode) {
+    if (this.top.data === searchValue){
+      return currentNode
+    }
+    currentNode = this.top.next
+  }
 }
